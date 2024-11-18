7be0463bc457af00a13827c7bc18bd9f8d2f0da8
SuniRein <sunirein@qq.com>
2024-11-18 21:08:00 +0800

    Parse accepted json data in backend.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,34 @@
 #include <filesystem>
 #include <iostream>
 
+#include "nlohmann/json.hpp"
 #include "webui.hpp"
 
 namespace fs = std::filesystem;
 
 fs::path const INDEX_PATH = YT_DLP_WEB_PATH;
 
+using Json = nlohmann::json;
+
 // Process submitted URL
-void handle_submit_url(webui::window::event* event) {}
+void handle_submit_url(webui::window::event* event)
+{
+    auto json = event->get_string_view();
+    auto data = Json::parse(json);
+
+    std::string action      = data["action"];
+    std::string url_input   = data["url_input"];
+    bool        audio_only  = data["audio_only"];
+    std::string quality     = data["quality"];
+    std::string output_path = data["output_path"];
+
+    std::clog << "== Accept request ==\n"
+              << "action: " << action << "\n"
+              << "url_input: " << url_input << "\n"
+              << "audio_only: " << audio_only << "\n"
+              << "quality: " << quality << "\n"
+              << "output_path: " << output_path << "\n";
+}
 
 int main(int argc, char* argv[])
 {
--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,15 @@
 set_project("yt-dlp-web")
 set_version("0.0.1")
 
+add_rules("mode.debug", "mode.release")
+
 add_requires("webui")
+add_requires("nlohmann_json") -- json parser
 
 target("main", function(target)
 	set_kind("binary")
 	add_files("src/*.cpp")
 	add_defines('YT_DLP_WEB_PATH="$(projectdir)/web"')
 	add_packages("webui")
+	add_packages("nlohmann_json")
 end)
