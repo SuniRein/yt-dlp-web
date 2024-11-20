1279ce5b67e58120e3f3db4539b09a35db629b84
SuniRein <sunirein@qq.com>
2024-11-20 16:50:00 +0800

    Add formats preview.

--- web/main.js
+++ web/main.js
@@ -59,6 +59,82 @@ function previewMediaInfo(data) {
     thumbnail.src = data.thumbnail;
     thumbnail.referrerPolicy = "no-referrer"; // Some sites block requests with referrer
     preview_area.appendChild(thumbnail);
+
+    // Show formats
+    const formats = analyzeFormats(data.formats);
+    preview_area.appendChild(formats);
+}
+
+function analyzeFormats(formats) {
+    // Create format table
+    const formats_table = document.createElement("table");
+    formats_table.classList.add("formats");
+
+    // Create table header
+    const thead = document.createElement("thead");
+
+    const header_row = document.createElement("tr");
+    const headers = [
+        "Format",
+        "Extension",
+        "Resolution",
+        "FPS",
+        "Size",
+        "TBR",
+        "Protocol",
+        "VCodec",
+        "VBR",
+        "ACodec",
+        "ABR",
+    ];
+    header_row.innerHTML = headers
+        .map((header) => `<th>${header}</th>`)
+        .join("");
+
+    thead.appendChild(header_row);
+    formats_table.appendChild(thead);
+
+    // Create table body
+    const tbody = document.createElement("tbody");
+
+    formats.forEach((format) => {
+        const row = document.createElement("tr");
+
+        const data = [
+            format.format_id,
+            format.ext,
+            format.resolution,
+            format.fps,
+            bytesToSize(format.filesize_approx),
+            format.tbr,
+            format.protocol,
+            hideIfEmpty(format.vcodec),
+            hideIfEmpty(format.vbr),
+            hideIfEmpty(format.acodec),
+            hideIfEmpty(format.abr),
+        ];
+        row.innerHTML = data.map((value) => `<td>${value || ""}</td>`).join("");
+
+        tbody.appendChild(row);
+    });
+
+    formats_table.appendChild(tbody);
+
+    return formats_table;
+}
+
+function bytesToSize(bytes) {
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    let i = 0;
+    while (bytes > 1024 && i < sizes.length) {
+        bytes /= 1024;
+        i++;
+    }
+    return `${bytes.toFixed(2)} ${sizes[i]}`;
+}
+
+function hideIfEmpty(value) {
+    return value == 0 || value == "none" ? "" : value;
 }
 
 function clearPreview() {
