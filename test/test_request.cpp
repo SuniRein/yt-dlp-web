c10287214f54fa2e678db25a1a924e51da5f0e71
SuniRein <sunirein@qq.com>
2024-11-28 21:33:03 +0800

    Break tests into smaller test units.

--- test/test_request.cpp
+++ test/test_request.cpp
@@ -12,6 +12,19 @@ using Json = nlohmann::json;
 
 using namespace std::string_literals;
 
+namespace
+{
+
+auto make_args(std::string_view json)
+{
+    Json data = Json::parse(json);
+    data.emplace("action", "preview");
+    data.emplace("url_input", "http://example.com");
+
+    Request request(data.dump());
+    return request.args;
+}
+
 MATCHER_P(HasOption, name, "has argument \""s + name + "\"")
 {
     return std::find(arg.begin(), arg.end(), name) != arg.end();
@@ -30,6 +43,8 @@ MATCHER_P2(HasArgumentOption, name, value, "has argument option \""s + name + "\
     }
     return false;
 }
+
+}  // anonymous namespace
 
 TEST(Request, ParseValidJSON)
 {
@@ -87,85 +102,82 @@ TEST(Request, InvalidJSON)
 
 TEST(Request, CookiesOptions)
 {
-    std::string json = R"({
-        "action": "preview",
-        "url_input": "https://example.com/video",
+    auto args = make_args(R"({
         "cookies_from_browser": "chrome",
         "cookies_from_file": "/tmp/cookies.txt"
-    })";
+    })");
 
-    Request request(json);
-
-    EXPECT_THAT(request.args, HasArgumentOption("--cookies-from-browser", "chrome"));
-    EXPECT_THAT(request.args, HasArgumentOption("--cookies", "/tmp/cookies.txt"));
+    EXPECT_THAT(args, HasArgumentOption("--cookies-from-browser", "chrome"));
+    EXPECT_THAT(args, HasArgumentOption("--cookies", "/tmp/cookies.txt"));
 }
 
-TEST(Request, NetworkOptions)
+TEST(Request, CommonNetworkOptions)
 {
-    std::string json = R"({
-        "action": "preview",
-        "url_input": "https://example.com/video",
+    auto args = make_args(R"({
         "proxy": "socks5://127.0.0.1:7890",
         "socket_timeout": "10",
         "source_address": "1.2.3.4",
-        "force_ip_protocol": "ipv6",
         "enable_file_urls": true
-    })";
+    })");
 
-    Request request(json);
-
-    EXPECT_THAT(request.args, HasArgumentOption("--proxy", "socks5://127.0.0.1:7890"));
-    EXPECT_THAT(request.args, HasArgumentOption("--socket-timeout", "10"));
-    EXPECT_THAT(request.args, HasArgumentOption("--source-address", "1.2.3.4"));
-    EXPECT_THAT(request.args, HasOption("--force-ipv6"));
-    EXPECT_THAT(request.args, HasOption("--enable-file-urls"));
+    EXPECT_THAT(args, HasArgumentOption("--proxy", "socks5://127.0.0.1:7890"));
+    EXPECT_THAT(args, HasArgumentOption("--socket-timeout", "10"));
+    EXPECT_THAT(args, HasArgumentOption("--source-address", "1.2.3.4"));
+    EXPECT_THAT(args, HasOption("--enable-file-urls"));
 }
 
-TEST(Request, VideoSelectionOptions)
+TEST(Request, ForceIpProtocolOption)
 {
-    std::string json = R"({
-        "action": "preview",
-        "url_input": "https://example.com/video",
-        "playlist_indices": "1,2:3,-6:-1,-5::0",
-        "filesize_min": "100K",
-        "filesize_max": "0.2M",
-        "date": "20220101",
-        "date_before": "now",
-        "date_after": "today-2week",
+    EXPECT_THAT(make_args(R"({"force_ip_protocol": "ipv4"})"), HasOption("--force-ipv4"));
+    EXPECT_THAT(make_args(R"({"force_ip_protocol": "ipv6"})"), HasOption("--force-ipv6"));
+}
+
+TEST(Request, PlaylistItemsOption)
+{
+    EXPECT_THAT(make_args(R"({"playlist_indices": "1,2:3,-6:-1,-5::0"})"), HasArgumentOption("--playlist-items", "1,2:3,-6:-1,-5::0"));
+}
+
+TEST(Request, FilesizeOption)
+{
+    EXPECT_THAT(make_args(R"({"filesize_min": "100K"})"), HasArgumentOption("--min-filesize", "100K"));
+    EXPECT_THAT(make_args(R"({"filesize_max": "0.2M"})"), HasArgumentOption("--max-filesize", "0.2M"));
+}
+
+TEST(Request, DateOption)
+{
+    EXPECT_THAT(make_args(R"({"date": "20220101"})"), HasArgumentOption("--date", "20220101"));
+    EXPECT_THAT(make_args(R"({"date_before": "now"})"), HasArgumentOption("--datebefore", "now"));
+    EXPECT_THAT(make_args(R"({"date_after": "today-2week"})"), HasArgumentOption("--dateafter", "today-2week"));
+}
+
+TEST(Request, FiltersOption)
+{
+    auto args = make_args(R"({
         "filters": [
             "!is_live",
             "like_count>?100 & description~='(?i)\\bcats \\& dogs\\b'"
-        ],
+        ]
+    })");
+
+    EXPECT_THAT(args, HasArgumentOption("--match-filters", "!is_live"));
+    EXPECT_THAT(args, HasArgumentOption("--match-filters", R"(like_count>?100 & description~='(?i)\bcats \& dogs\b')"));
+}
+
+TEST(Request, StopFiltersOption)
+{
+    auto args = make_args(R"({
         "stop_filters": [
             "duration > 600",
             "duration < 3600"
-        ],
-        "is_playlist": "no"
-    })";
+        ]
+    })");
 
-    Request request(json);
-
-    EXPECT_THAT(request.args, HasArgumentOption("--playlist-items", "1,2:3,-6:-1,-5::0"));
-    EXPECT_THAT(request.args, HasArgumentOption("--min-filesize", "100K"));
-    EXPECT_THAT(request.args, HasArgumentOption("--max-filesize", "0.2M"));
-    EXPECT_THAT(request.args, HasArgumentOption("--date", "20220101"));
-    EXPECT_THAT(request.args, HasArgumentOption("--datebefore", "now"));
-    EXPECT_THAT(request.args, HasArgumentOption("--dateafter", "today-2week"));
-    EXPECT_THAT(request.args, HasArgumentOption("--match-filters", "!is_live"));
-    EXPECT_THAT(request.args, HasArgumentOption("--match-filters", R"(like_count>?100 & description~='(?i)\bcats \& dogs\b')"));
-    EXPECT_THAT(request.args, HasArgumentOption("--break-match-filters", "duration > 600"));
-    EXPECT_THAT(request.args, HasArgumentOption("--break-match-filters", "duration < 3600"));
-    EXPECT_THAT(request.args, HasOption("--no-playlist"));
+    EXPECT_THAT(args, HasArgumentOption("--break-match-filters", "duration > 600"));
+    EXPECT_THAT(args, HasArgumentOption("--break-match-filters", "duration < 3600"));
 }
 
 TEST(Request, IsPlaylistOption)
 {
-    {
-        Request request(R"({"action": "preview", "url_input": "https://example.com/playlist", "is_playlist": "no"})");
-        EXPECT_THAT(request.args, HasOption("--no-playlist"));
-    }
-    {
-        Request request(R"({"action": "preview", "url_input": "https://example.com/playlist", "is_playlist": "yes"})");
-        EXPECT_THAT(request.args, HasOption("--yes-playlist"));
-    }
+    EXPECT_THAT(make_args(R"({"is_playlist": "no" })"), HasOption("--no-playlist"));
+    EXPECT_THAT(make_args(R"({"is_playlist": "yes"})"), HasOption("--yes-playlist"));
 }
