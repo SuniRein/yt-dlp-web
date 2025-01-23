661d5803e9b66de6d53ebd603cebde0f84459c65
SuniRein <sunirein@qq.com>
2025-01-23 17:33:25 +0800

    Add global notification api in web page.

---
+++ web/src/components/NotificationDisplay.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useNotification } from 'naive-ui';
+
+import { registerNotificationApi } from '@/utils/notification';
+
+onMounted(() => {
+    registerNotificationApi(useNotification());
+});
+</script>
+
+<template>
+    <!-- An empty component just for registering notification api. -->
+    <template />
+</template>
