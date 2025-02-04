475889283baecb0e1563eac07728a7f2b8177cca
SuniRein <sunirein@qq.com>
2025-02-04 13:59:54 +0800

    Use NDataTable to show log info.

--- web/src/store/__tests__/log.test.ts
+++ web/src/store/__tests__/log.test.ts
@@ -1,15 +1,30 @@
 import { useLogStore } from '@/store/log';
-import { test, expect, beforeEach } from 'vitest';
+import { test, expect, beforeEach, afterEach, vi } from 'vitest';
 import { setActivePinia, createPinia } from 'pinia';
 
-beforeEach(() => setActivePinia(createPinia()));
+const date = new Date(2021, 0, 1, 0, 0, 0);
+
+beforeEach(() => {
+    setActivePinia(createPinia());
+    vi.useFakeTimers();
+    vi.setSystemTime(date);
+});
+
+afterEach(() => {
+    vi.useRealTimers();
+});
 
 test('log info', () => {
     const log = useLogStore();
 
     log.log('info', 'Hello, World!');
 
-    expect(log.str).toBe('[Info] Hello, World!\n');
+    expect(log.store).toHaveLength(1);
+    expect(log.store[0]).toEqual({
+        time: date,
+        level: 'info',
+        message: 'Hello, World!',
+    });
 });
 
 test('log warning', () => {
@@ -17,7 +32,12 @@ test('log warning', () => {
 
     log.log('warning', 'Hello, World!');
 
-    expect(log.str).toBe('[Warning] Hello, World!\n');
+    expect(log.store).toHaveLength(1);
+    expect(log.store[0]).toEqual({
+        time: date,
+        level: 'warning',
+        message: 'Hello, World!',
+    });
 });
 
 test('log multiple messages', () => {
@@ -26,7 +46,17 @@ test('log multiple messages', () => {
     log.log('error', 'Hello, World!');
     log.log('debug', 'Another Hello, World!');
 
-    expect(log.str).toBe('[Error] Hello, World!\n[Debug] Another Hello, World!\n');
+    expect(log.store).toHaveLength(2);
+    expect(log.store[0]).toEqual({
+        time: date,
+        level: 'error',
+        message: 'Hello, World!',
+    });
+    expect(log.store[1]).toEqual({
+        time: date,
+        level: 'debug',
+        message: 'Another Hello, World!',
+    });
 });
 
 test('clear log', () => {
@@ -35,5 +65,5 @@ test('clear log', () => {
     log.log('warning', 'Hello, World!');
     log.clear();
 
-    expect(log.str).toBe('');
+    expect(log.store).toHaveLength(0);
 });
--- web/src/views/LogView.vue
+++ web/src/views/LogView.vue
@@ -1,9 +1,48 @@
 <script setup lang="ts">
-import { NFloatButton, NIcon } from 'naive-ui';
+import { h, capitalize, computed } from 'vue';
+import { NFloatButton, NIcon, NDataTable, NTag, NEmpty } from 'naive-ui';
 import { useLogStore } from '@/store/log';
 import ClearIcon from '@vicons/fluent/Broom16Regular';
 
 const log = useLogStore();
+
+function renderLevel(level: Row['level']) {
+    const levelMap = {
+        info: 'info',
+        warning: 'warning',
+        error: 'error',
+        debug: 'default',
+    } as const;
+
+    return h(NTag, { type: levelMap[level], round: true, bordered: false }, { default: () => capitalize(level) });
+}
+
+const columns = [
+    {
+        title: 'Time',
+        key: 'time',
+        render(row: Row) {
+            return new Date(row.time).toLocaleString();
+        },
+    },
+    {
+        title: 'Level',
+        key: 'level',
+        render(row: Row) {
+            return renderLevel(row.level);
+        },
+    },
+    {
+        title: 'Message',
+        key: 'message',
+        render(row: Row) {
+            return h('pre', { style: { margin: 0 } }, row.message);
+        },
+    },
+];
+
+const data = computed(() => log.store);
+type Row = (typeof data.value)[0];
 </script>
 
 <template>
@@ -21,8 +60,10 @@ const log = useLogStore();
             <NIcon :component="ClearIcon" />
         </NFloatButton>
 
-        <div style="margin: auto 16px" data-test="log-content">
-            <pre>{{ log.str }}</pre>
-        </div>
+        <NDataTable :columns :data data-test="log-content">
+            <template #empty>
+                <NEmpty description="No log." />
+            </template>
+        </NDataTable>
     </div>
 </template>
