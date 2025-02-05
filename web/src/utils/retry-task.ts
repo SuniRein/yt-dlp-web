24f93cc4e7b46b6d55b637d03b8f67d17f5287a0
SuniRein <sunirein@qq.com>
2025-02-05 13:42:40 +0800

    Send log when retrying task. Fix wrong task status `interrupt` when a task is retried.

--- web/src/utils/retry-task.ts
+++ web/src/utils/retry-task.ts
@@ -1,18 +1,23 @@
 import { useTasksStore, type Task } from '@/store/tasks';
 import { useNotification } from '@/utils/notification';
+import { useLogStore } from '@/store/log';
 
 export async function retryTask(id: Task['id']) {
+    const log = useLogStore();
     const tasks = useTasksStore();
 
     const task = tasks.value.get(id);
     if (!task) {
+        log.log('warning', `Try to retry task ${id}, but not found.`);
         return;
     }
 
     tasks.remove(id);
 
     const newId = parseInt(await webui.handleRequest(JSON.stringify({ ...task.request, action: task.type })));
-    tasks.append({ ...task, id: newId });
+    tasks.append({ ...task, id: newId, status: 'running' });
+
+    log.log('info', `Retried task ${id} -> task ${newId}.`);
 
     useNotification().info({
         title: `Task ${id} has been retried.`,
