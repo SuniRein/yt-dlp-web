994f16e13f6fef11f33859ed5e798847d97004fe
SuniRein <sunirein@qq.com>
2025-02-05 21:51:48 +0800

    Add cmdline parser.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include "app.h"
+#include "syscmdline/parser.h"
+#include "syscmdline/system.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-    auto& app = ytweb::App::instance();
-    app.init();
-    app.run();
+    SysCmdLine::Command root_command("yt-dlp-web");
+    root_command.addHelpOption();
+    root_command.setHandler([](SysCmdLine::ParseResult const& result) {
+        auto& app = ytweb::App::instance();
+        app.init();
+        app.run();
+
+        return 0;
+    });
+
+    SysCmdLine::Parser parser(root_command);
+    return parser.invoke(SysCmdLine::commandLineArguments());
 }
