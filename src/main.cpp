a3c0c9757e8acfd3f5ff20fef99f2a429e3ac7a3
SuniRein <sunirein@qq.com>
2025-02-06 13:13:26 +0800

    Add alias --browser and --webview.

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,9 @@ namespace SCL = SysCmdLine;
 
 int main()
 {
-    SCL::Argument runtime_argument("runtime");
-    runtime_argument.setExpectedValues({
+    auto runtime_option = SCL::Option({"--runtime", "-r"}, "Set the runtime to use.");
+    runtime_option.setRequired(false);
+    runtime_option.addArgument(SCL::Argument("runtime").expect({
         "no",
         "browser",
         "chrome",
@@ -23,20 +24,30 @@ int main()
         "yandex",
         "chromium",
         "webview",
-    });
+    }));
 
-    SCL::Option runtime_option({"--runtime", "-r"}, "Set the runtime to use");
-    runtime_option.setRequired(false);
-    runtime_option.addArgument(runtime_argument);
+    SCL::Option browser_option({"--browser", "-b"}, "Show in browser. Alias for '--runtime browser'.");
+    browser_option.setRequired(false);
+
+    SCL::Option webview_option({"--webview", "-w"}, "Show in webview. Alias for '--runtime webview'. (Default)");
+    webview_option.setRequired(false);
 
     SCL::Command root_command("yt-dlp-web");
     root_command.addHelpOption();
-    root_command.addOption(runtime_option);
+    root_command.addOptions({runtime_option, browser_option, webview_option});
     root_command.setHandler([&](SCL::ParseResult const& result) {
         auto& app = ytweb::App::instance();
         app.init();
 
-        if (result.isOptionSet(runtime_option))
+        if (result.isOptionSet(browser_option))
+        {
+            app.set_runtime(ytweb::Runtime::AnyBrowser);
+        }
+        else if (result.isOptionSet(webview_option))
+        {
+            app.set_runtime(ytweb::Runtime::Webview);
+        }
+        else if (result.isOptionSet(runtime_option))
         {
             auto runtime = ytweb::get_runtime(result.valueForOption(runtime_option).toString());
             if (runtime.has_value())
